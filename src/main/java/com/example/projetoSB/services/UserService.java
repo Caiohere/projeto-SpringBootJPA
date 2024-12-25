2bc68829c0044c29b58bd0488765d7ebbc241ff4
CaioHere <caiohenry422@gmail.com>
2024-12-25 18:23:17 -0300

    Services

---
+++ src/main/java/com/example/projetoSB/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.projetoSB.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Service;
+
+import com.example.projetoSB.entities.User;
+import com.example.projetoSB.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> FindAll () {
+		return repository.findAll();
+	}
+	
+	public User findById (Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
