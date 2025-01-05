067584a89b471f7f226f9bb6a315cab9c83e27b0
CaioHere <caiohenry422@gmail.com>
2025-01-05 18:42:25 -0300

    Payment Entity, One-To-One Association

--- src/main/java/com/example/projetoSB/entities/Order.java
+++ src/main/java/com/example/projetoSB/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.example.projetoSB.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +33,9 @@ public class Order implements Serializable {
 	private Instant moment;
 	
 	private Integer orderStatus;
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	@ManyToOne
 	@JoinColumn(name="client_id")
@@ -89,6 +94,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	@Override
 	public int hashCode() {
---
+++ src/main/java/com/example/projetoSB/entities/Payment.java
@@ -0,0 +1,71 @@
+package com.example.projetoSB.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Instant moment;
+	@Id
+	@MapsId
+	@OneToOne
+	private Order order;
+	
+	public Payment() {
+	}
+
+	public Payment(Long id, Instant moment, Order order) {
+		super();
+		this.moment = moment;
+		this.order = order;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(order, other.order);
+	}
+
+	
+	
+	
+	
+	
+}
